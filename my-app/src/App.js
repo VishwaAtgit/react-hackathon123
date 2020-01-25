47fc7a73ecacc3a79bead87a5fe66be5fa3fef6f
unknown <maurya.jyotsna323@gmail.com>
2020-01-25 10:15:01 +0000

    data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import Home from './components/HomeComponent';
+// import HotelList from './components/HotelListComponent';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
@@ -22,6 +23,7 @@ function App() {
         </a>
       </header> */}
       <Home/>
+{/* <HotelList/> */}
     </div>
   );
 }
--- my-app/src/components/HotelListComponent.js
+++ my-app/src/components/HotelListComponent.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import hotelDetails from '../assets/jsons/hotelDetails.json';
+import { render } from '@testing-library/react';
 import ReactDOM from 'react-dom';
 
-// class HotelList extends React.Component {
-//   render() {
-//     return (
-//         <>
-//         <div className="container">
-
-//         </div>
-//         </>
-//     )
-//   }
-// }
 const HotelList = () => {
+  const [data,setData]=useState();
+  const data=
+  hotelDetails.map((num)=><li>{num}</li>)
+  const d = JSON.parse(data);
 
+
+return(
+  // <div className="container">
+  <div>{d.id}</div>
+  // </div>
+
+  // <table>
+  //   <thead>
+  //     <th>hotel id</th>
+  //     </thead>
+  //     <tbody>
+  //       <tr>
+  //         <td>{data.id}</td>
+  //       </tr>
+  //     </tbody>
+  // </table>
+)
 }
+
 
 export default HotelList;
