02f797578d4b4824ab60757ee572c2d3c4da18c8
unknown <bhagyashree.ssc@gmail.com>
2020-01-25 08:16:13 +0000

    search component

---
+++ my-app/src/components/HomeComponent.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Search extends React.Component {
+  render() {
+    return (
+        <>
+        <div className="container">
+        <form action="#" method="post" novalidate="novalidate" className="search-block">
+            <div className="row">
+                <div className="col-lg-12">
+                    <div className="row">
+                        <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <input type="text" className="form-control search-slt" />
+                        </div>
+                         <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <select className="form-control search-slt" id="exampleFormControlSelect1">
+                                <option>Select Drop State</option>
+                                <option>Example one</option>
+                                <option>Example one</option>
+                                <option>Example one</option>
+                                <option>Example one</option>
+                                <option>Example one</option>
+                                <option>Example one</option>
+                            </select>
+                        </div>
+                          <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <select className="form-control search-slt" id="exampleFormControlSelect1">
+                                <option>Select Drop City</option>
+                                <option>Example one</option>
+                                <option>Example one</option>
+                                <option>Example one</option>
+                                <option>Example one</option>
+                                <option>Example one</option>
+                                <option>Example one</option>
+                            </select>
+                        </div>
+                        <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <button type="button" className="btn btn-primary wrn-btn">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>         
+        </div>
+        </>
+    )
+  }
+}
+
+export default Search;
