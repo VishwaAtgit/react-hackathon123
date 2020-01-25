151b06bcffba1a74d39b96e58184a77f3b1df4dc
unknown <bhagyashree.ssc@gmail.com>
2020-01-25 10:20:51 +0000

    search map for country

--- my-app/src/components/HomeComponent.js
+++ my-app/src/components/HomeComponent.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import hotelDetails from '../assets/jsons/hotelDetails.json';
+import location from '../assets/jsons/location.json';
 
 class Home extends React.Component {
     constructor(props){
-    super(props);
-    console.log(hotelDetails[0].id);
+    super(props);  
+    this.state = {data:hotelDetails, locationDetails: location};
     }
    
   render() {
     return (
         <>
         <div className="container">
+       
         <form action="#" method="post" novalidate="novalidate" className="search-block">
             <div className="row">
                 <div className="col-lg-12">
@@ -21,24 +23,25 @@ class Home extends React.Component {
                         </div>
                          <div className="col-lg-3 col-md-3 col-sm-12 p-0">
                             <select className="form-control search-slt" id="exampleFormControlSelect1">
-                                <option>Select Drop State</option>
-                                <option>Example one</option>
-                                <option>Example one</option>
-                                <option>Example one</option>
-                                <option>Example one</option>
-                                <option>Example one</option>
-                                <option>Example one</option>
+                                <option>Select State</option>
+                                {
+                                    this.state.locationDetails.map(function(item, i){
+                                    console.log(item.state);
+                                        return <option value={item.state}>{item.state}</option>
+                                    })
+                                    }
+                                
                             </select>
                         </div>
                           <div className="col-lg-3 col-md-3 col-sm-12 p-0">
                             <select className="form-control search-slt" id="exampleFormControlSelect1">
                                 <option>Select Drop City</option>
-                                <option>Example one</option>
-                                <option>Example one</option>
-                                <option>Example one</option>
-                                <option>Example one</option>
-                                <option>Example one</option>
-                                <option>Example one</option>
+                                {
+                                    this.state.data.map(function(item, i){
+                                    console.log(item.id);
+                                        return <option value={item.id}>{item.id}</option>
+                                    })
+                                    }
                             </select>
                         </div>
                         <div className="col-lg-3 col-md-3 col-sm-12 p-0">
