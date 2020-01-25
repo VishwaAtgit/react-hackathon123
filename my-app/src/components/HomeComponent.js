970619e99a745fdd51cc8344c392e854dbb0bad3
unknown <maurya.jyotsna323@gmail.com>
2020-01-25 08:30:39 +0000

    data-added

--- my-app/src/components/HomeComponent.js
+++ my-app/src/components/HomeComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Search extends React.Component {
+class Home extends React.Component {
   render() {
     return (
         <>
@@ -48,4 +48,4 @@ class Search extends React.Component {
   }
 }
 
-export default Search;
+export default Home;
--- my-app/src/components/HotelListComponent.js
+++ my-app/src/components/HotelListComponent.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class HotelList extends React.Component {
-  render() {
-    return (
-        <>
-        <div className="container">
-            <img src="" />
-            <p>Hotel Name</p>
-            <p>Rating: **</p>
-            <p>Address: Some Address</p>
-        </div>
-        </>
-    )
-  }
+// class HotelList extends React.Component {
+//   render() {
+//     return (
+//         <>
+//         <div className="container">
+
+//         </div>
+//         </>
+//     )
+//   }
+// }
+const HotelList = () => {
+
 }
 
 export default HotelList;
