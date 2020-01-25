b5ab752839cea0032e418459d04c7815a939a313
unknown <maurya.jyotsna323@gmail.com>
2020-01-25 12:26:32 +0000

    seaching

--- my-app/src/components/HomeComponent.js
+++ my-app/src/components/HomeComponent.js
@@ -2,72 +2,81 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import location from '../assets/jsons/location.json';
 import hotelDetails from '../assets/jsons/hotelDetails.json';
+import HotelViewComponent from './HotelViewComponent';
 
 class Home extends React.Component {
-    constructor(props){
-    super(props);  
-    this.state = {data:hotelDetails, locationDetails: location};
-  
-    this.search = this.search.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = {hotelDetails:hotelDetails, locationDetails: location, hotelId:"", hotelName:""};
+    constructor(props) {
+        super(props);
+        this.search = this.search.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.onInputChange = this.onInputChange.bind(this);
+        this.state = { hotelDetails: hotelDetails, locationDetails: location, hotelId: "", hotelName: "", stateName: "", filteredData:hotelDetails};
     }
-   search(){
-    //    this.setState({ hotelId:""});
-       const filteredData = this.state.hotelDetails.filter((element)=>element.state===this.state.hotelName);
-       console.log("sdfsdf",filteredData);
-       
-       console.log(this.state.hotelName);
-   }
-   handleChange = (event) => {     
-     this.setState({ hotelName: event.target.value });
-  };
-  render() {
-    const {  value } = this.state;
-    return (
-        <>
-        <div className="container">
-       
-        <form action="#" method="post" novalidate="novalidate" className="search-block">
-            <div className="row">
-                <div className="col-lg-12">
-                    <div className="row">
-                        <div className="col-lg-3 col-md-3 col-sm-12 p-0">
-                            <input type="text" className="form-control search-slt" placeholder="Search by hotel name" />
+    search() {
+        if (this.state.stateName) {
+            this.state.filteredData = this.state.hotelDetails.filter((element) => element.state === this.state.stateName);
+            this.setState({stateName :"", hotelName :""});
+            this.forceUpdate();
+           
+        }
+        if (this.state.hotelName) {
+            this.state.filteredData = this.state.hotelDetails.filter((element) => element.name === this.state.hotelName);
+            this.setState({hotelName :"", stateName :""});
+            this.forceUpdate();
+            
+        }
+    }
+    handleChange = (event) => {
+        this.setState({ stateName: event.target.value });
+    };
+    onInputChange = (event) => {
+        this.setState({ hotelName: event.target.value });
+    };
+    render() {
+        const { value } = this.state;
+        return (
+                <div className="container">
+
+                    <form action="#" method="post" novalidate="novalidate" className="search-block">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <div className="row">
+                                    <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                                        <input type="text" className="form-control search-slt" placeholder="Search by hotel name" onChange={this.onInputChange} />
+                                    </div>
+                                    <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                                        <select className="form-control search-slt" id="exampleFormControlSelect1" onChange={this.handleChange} value={value}>
+                                            <option>Select State</option>
+                                            {
+                                                this.state.locationDetails.map(function (item, i) {
+                                                    return <option value={item.state}>{item.state}</option>
+                                                })
+                                            }
+
+                                        </select>
+                                    </div>
+                                    <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                                        <select className="form-control search-slt" id="exampleFormControlSelect1">
+                                            <option>Select Drop City</option>
+                                            {
+                                                this.state.locationDetails.map(function (item, i) {
+
+                                                    return <option value={item.city}>{item.city}</option>
+                                                })
+                                            }
+                                        </select>
+                                    </div>
+                                    <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                                        <button type="button" className="btn btn-primary wrn-btn" onClick={this.search}>Search</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                         <div className="col-lg-3 col-md-3 col-sm-12 p-0">
-                            <select className="form-control search-slt" id="exampleFormControlSelect1"  onChange={this.handleChange} value={value}>
-                                <option>Select State</option>
-                                {
-                                    this.state.locationDetails.map(function(item, i){
-                                        return <option value={item.state}>{item.state}</option>
-                                    })
-                                    }
-                                
-                            </select>
-                        </div>
-                          <div className="col-lg-3 col-md-3 col-sm-12 p-0">
-                            <select className="form-control search-slt" id="exampleFormControlSelect1">
-                                <option>Select Drop City</option>
-                                {
-                                    this.state.locationDetails.map(function(item, i){
-                                    
-                                        return <option value={item.city}>{item.city}</option>
-                                    })
-                                    }
-                            </select>
-                        </div>
-                        <div className="col-lg-3 col-md-3 col-sm-12 p-0">
-                            <button type="button" className="btn btn-primary wrn-btn" onClick={this.search}>Search</button>
-                        </div>
-                    </div>
+                    </form>
+                    <HotelViewComponent hotelData={this.state.filteredData}/>
                 </div>
-            </div>
-        </form>         
-        </div>
-        </>
-    )
-  }
+        )
+    }
 }
 
 export default Home;
--- my-app/src/components/HotelViewComponent.js
+++ my-app/src/components/HotelViewComponent.js
@@ -1,36 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import hotelDetails from '../assets/jsons/hotelDetails.json';
-class HotelViewComponent extends React.Component {
-    constructor(props){
-        super(props);  
-        this.state = {data:hotelDetails};
-        }
-  render() {
-    return (
-        <div className="container p-0">       
-        <div className="HotelListView">
-        <div className="col-lg-4 col-md-4 col-sm-12 p-0">
-        <img src="https://pbs.twimg.com/media/EGHYvttU4AAYKb7?format=jpg&name=large" className="d-block w-100" alt="..." />
-         </div>
-         <div className="col-lg-6 col-md-6 col-sm-12 hotelDetails">
-             <p><b>HotelName :</b> Hotel Name   
-                 <span class="float-right"><i class="text-warning fa fa-star"></i><i className="text-warning fa fa-star"></i></span></p>
-             <p><b>Address: </b>Some Address</p>
-             <p><b>Room Type: </b>AC | Non-AC</p>
-            
-            </div>
-            <div className="col-lg-2 col-md-2 col-sm-12 ">
-            <button className="btn btn-xlarge">View Details</button>
-            <button className="btn btn-xlarge">Book Now</button>
-            
-            </div>
-        </div>
+// import hotelDetails from '../assets/jsons/hotelDetails.json';
+// class HotelViewComponent extends React.Component {
+//     constructor(props){
+//         super(props);  
+//         this.state = {data:props.hotelData};
+//         }
+//   render() {
+//     return (
+//         <div className="container p-0">       
+       
+//         <div className="container p-0">
+//         {
+//             this.state.data.map(function(item, i){
+//                 console.log(item);
+//              return (
+//                 <div className="HotelListView">
+//                 <div className="col-lg-4 col-md-4 col-sm-12 p-0">
+//                 <img src="https://pbs.twimg.com/media/EGHYvttU4AAYKb7?format=jpg&name=large" class="d-block w-100" alt="..." />
+//                  </div>
+//                  <div className="col-lg-6 col-md-6 col-sm-12 hotelDetails">
+//                      <p><b>HotelName :</b> {item.name}   
+//                          <span class="float-right"><i class="text-warning fa fa-star"></i><b className="ratings">{item.rating}</b></span></p>
+//                      <p><b>Address: </b> {item.address}, {item.state}, {item.city} </p>
+//                      <p><b>Room Type: </b>{item.hotelType}</p>
+                    
+//                     </div>
+//                     <div className="col-lg-2 col-md-2 col-sm-12 ">
+//                     <button className="btn btn-xlarge">View Details</button>
+//                     <button className="btn btn-xlarge">Book Now</button>
+                    
+//                     </div>
+//                 </div>
+//                 )
+//             })
+//         }       
+//         </div>
+//         </div>
+//     )
+//   }
+// }
+const HotelViewComponent = (props) => {
+  console.log("-------------------")
+  if (props != null && props.hotelData != null) {
+return (  
         <div className="container p-0">
         {
-            this.state.data.map(function(item, i){
+            props.hotelData.map(function(item, i){
                 console.log(item);
              return (
+              //  <div>{item.name}</div>
                 <div className="HotelListView">
                 <div className="col-lg-4 col-md-4 col-sm-12 p-0">
                 <img src="https://pbs.twimg.com/media/EGHYvttU4AAYKb7?format=jpg&name=large" class="d-block w-100" alt="..." />
@@ -52,8 +71,7 @@ class HotelViewComponent extends React.Component {
             })
         }       
         </div>
-        </div>
-    )
+)
   }
 }
 
