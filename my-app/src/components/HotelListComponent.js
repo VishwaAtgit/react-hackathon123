932e2a61c0e5797061f486812796019000d57be9
unknown <bhagyashree.ssc@gmail.com>
2020-01-25 08:00:48 +0000

    List component

---
+++ my-app/src/components/HotelListComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HotelList extends React.Component {
+  render() {
+    return (
+        <>
+        <div className="container">
+            <img src="" />
+            <p>Hotel Name</p>
+            <p>Rating: **</p>
+            <p>Address: Some Address</p>
+        </div>
+        </>
+    )
+  }
+}
+
+export default HotelList;
